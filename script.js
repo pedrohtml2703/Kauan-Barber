199f77c2555afa6ecd807c754ad29572c7eee617
Pedrohtml <pedrohtml2703@gmail.com>
2026-05-22 22:24:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
+
+```javascript
 AOS.init({
   offset:0
 });
 
-const menuBtn =
-document.querySelector(".menu-btn");
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
 
-const navigation =
-document.querySelector(".navigation");
+menuBtn.addEventListener('click', () => {
 
-const navigationItems =
-document.querySelectorAll(".navigation a");
-
-menuBtn.addEventListener("click", () => {
-
-  menuBtn.classList.toggle("active");
-
-  navigation.classList.toggle("active");
+menuBtn.classList.toggle('active');
+navigation.classList.toggle('active');
 
 });
 
-window.addEventListener("scroll", function(){
+window.addEventListener('scroll', function(){
 
-  const header =
-  document.querySelector("header");
-
-  header.classList.toggle(
-    "sticky",
-    window.scrollY > 0
-  );
+const header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 0);
 
 });
-
-navigationItems.forEach((navigationItem) => {
-
-  navigationItem.addEventListener("click", () => {
-
-    menuBtn.classList.remove("active");
-
-    navigation.classList.remove("active");
-
-  });
-
-});
+```
